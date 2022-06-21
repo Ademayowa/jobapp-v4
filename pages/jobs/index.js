e12ed82183062bb4ebe66940e7216e9ea51d525b
Ademayowa <ademayowaadeniyi@gmail.com>
2022-06-21 19:43:09 +0100

    added jobs in pages folder

--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -1,7 +1,55 @@
-export default function JobsPage() {
+import fs from 'fs';
+import path from 'path';
+import Link from 'next/link';
+import matter from 'gray-matter';
+import { sortByDate } from '@/utils/index';
+import Hero from '@/components/Hero';
+import Job from '@/components/Job';
+import Layout from '@/components/Layout';
+import Search from '@/components/Search';
+import SectionTitle from '@/components/SectionTitle';
+
+export default function JobsPage({ jobs }) {
+  // console.log(jobs);
   return (
-    <div>
-      <h2 className='text-5xl'>Jobs page</h2>
-    </div>
+    <Layout title='Find Gigs'>
+      {/* <Hero />
+      <Search /> */}
+      <SectionTitle title='All Jobs' />
+
+      <div className='grid grid-cols-8 gap-x-7 px-10 md:px-12 md:mx-10'>
+        {jobs.map((job, index) => (
+          <Job key={index} job={job} />
+        ))}
+      </div>
+    </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('jobs'));
+
+  const jobs = files.map((filename) => {
+    // remove the .md extension from jobs
+    const slug = filename.replace('.md', '');
+
+    // gets all the contents of d md file
+    const markdownWithMeta = fs.readFileSync(
+      path.join('jobs', filename),
+      'utf-8'
+    );
+    // {data:frontmatter} => data here being renamed to frontmatter
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      jobs: jobs.sort(sortByDate),
+    },
+  };
 }
