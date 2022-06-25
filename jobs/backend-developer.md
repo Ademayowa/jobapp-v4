c2c9b1bae0607547491e7dfb4c0fae86c8f9c9c1
Ademayowa <ademayowaadeniyi@gmail.com>
2022-06-25 15:31:31 +0100

    displayed single job

--- jobs/backend-developer.md
+++ jobs/backend-developer.md
@@ -7,11 +7,10 @@ category: 'Backend'
 description: 'Perpetual Solution is a Canadian owned and operated company specializing in Website Development and E-Commerce solutions.'
 text: 'We are looking for a php developer who is looking to augment their existing workload. You are passionate about coding, solving problems, creating solutions and you keep current on trends and technology.'
 info: 'Job Requirements'
-title: 'Responsibilities'
 ---
-
-[apply for this role](https://perpetualsolution.com/job-detail.php?id=32),
 
 - Ecommerce experience.
 - Good verbal and written communications skills
 - One to three years of experience as a php backend developer
+
+  [apply for this role](https://perpetualsolution.com/job-detail.php?id=32),
--- pages/jobs/[slug].js
+++ pages/jobs/[slug].js
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import path from 'path';
+import { marked } from 'marked';
 import matter from 'gray-matter';
+import Link from 'next/link';
+import { LocationMarkerIcon } from '@heroicons/react/outline';
+import { BsArrowLeft } from 'react-icons/bs';
+import { MdMonetizationOn } from 'react-icons/md';
 import Layout from '@/components/Layout';
 
 export default function Job({
@@ -13,14 +18,49 @@ export default function Job({
     role,
     salary,
     text,
-    title,
   },
   slug,
   content,
 }) {
   return (
-    <Layout title='Job'>
-      <h2>{role}</h2>
+    <Layout title={role}>
+      <div className='py-5 shadow-lg bg-white rounded-lg m-5 md:m-10'>
+        <div className='px-10 md:mx-10 md:px-12 py-4'>
+          <div className='my-4'>
+            <Link href='/jobs'>
+              <a>
+                <BsArrowLeft className='w-5 h-5 my-5 cursor-pointer text-[#EF4444]' />
+              </a>
+            </Link>
+            <h2 className='text-blueColor text-lg md:text-2xl font-bold mt-2'>
+              {role}
+            </h2>
+
+            <div className='flex items-center mt-2 space-x-2'>
+              <LocationMarkerIcon className='w-5 h-5 text-blue-500 -ml-1' />
+              <p className='text-grayColor text-base md:text-lg'>{location}</p>
+            </div>
+
+            <div className='flex items-center mt-2 space-x-2'>
+              <MdMonetizationOn className='w-5 h-5 text-blue-500 -ml-1' />
+              <p className='text-grayColor text-base md:text-lg'>{salary}</p>
+            </div>
+          </div>
+        </div>
+        <hr className='border-b border-red-400' />
+
+        <div className='job-post px-10 md:mx-10 md:px-12 py-4 text-grayColor'>
+          <div className='mt-5 text-sm md:text-base !leading-9'>
+            <p className='max-w-4xl'>{text}</p>
+            <h4 className='my-4 font-bold'>{info}</h4>
+
+            <div className='flex items-center space-x-4'>
+              {/* html:marked - display markdown file as html in browser */}
+              <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
