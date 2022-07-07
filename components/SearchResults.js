31457f37740b1ff1cfa294fb7c91867568f69d70
Ademayowa <ademayowaadeniyi@gmail.com>
2022-07-07 07:38:48 +0100

    hides search results components:user does not type in the form

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,6 +1,9 @@
 import Job from './Job';
 
 export default function SearchResults({ results }) {
+  // Hides the search component result when nothing is being typed in d form by users
+  if (results.length === 0) return <></>;
+
   return (
     <div className='absolute top-20 right-0 md:right-10 z-10 border-2 border-blue-200 bg-white text-black w-full md:w-6/12 rounded-2xl mt-5'>
       <div className='p-10'>
